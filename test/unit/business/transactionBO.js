817a364c3019ecc3a4bcf7ba2469ebfaa9b5d68e
VitorDaynno <vitordaynno@gmail.com>
2018-08-30 18:18:13 -0300

    [SISSSG-215] Adição de testes de em transactions

--- test/unit/business/transactionBO.js
+++ test/unit/business/transactionBO.js
@@ -11,6 +11,34 @@ describe('TransactionBO', function(){
         transactionsDAO: transactionsDAO
     });
 
+    describe('add', function(){
+        it('Should return error because value does not informed', function(){
+            return transactionBO.add({description: 'Tênis', type: 'out', category: 'Vestuário', date: new Date(), formPayment: 'Débito'})
+                .then()
+                .catch(function(error){
+                    expect(error.code).to.be.equals(422);
+                    expect(error.message).to.be.equals('The entity should has a field Value');
+                });
+        });
+        it('Should return error because Description does not informed', function(){
+            return transactionBO.add({value: 33.9, type: 'out', category: 'Vestuário', date: new Date(), formPayment: 'Débito'})
+                .then()
+                .catch(function(error){
+                    expect(error.code).to.be.equals(422);
+                    expect(error.message).to.be.equals('The entity should has a field Description');
+                });
+        });
+        it('Should return error because out does not informed', function(){
+            return transactionBO.add({description: 'Tênis', value: 33.9, category: 'Vestuário', date: new Date(), formPayment: 'Débito'})
+                .then()
+                .catch(function(error){
+                    expect(error.code).to.be.equals(422);
+                    expect(error.message).to.be.equals('The entity should has a field Out');
+                });
+        });
+    });
+
+    //{description: 'Tênis', value: 99.0, type: 'out', category: 'Vestuário', date: new Date(), formPayment: 'Débito'}
     describe('getAll', function(){
         it('Should return zero transactions if userId does not exists', function(){
             return transactionBO.getAll({userId: 22})
