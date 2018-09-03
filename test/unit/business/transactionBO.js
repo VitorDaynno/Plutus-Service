b23f088cf05f83c21d5d43f98c22fb9ebc86b4d7
VitorDaynno <vitordaynno@gmail.com>
2018-09-03 19:43:58 -0300

    Adição de alguns testes

--- test/unit/business/transactionBO.js
+++ test/unit/business/transactionBO.js
@@ -12,24 +12,58 @@ describe('TransactionBO', function(){
     });
 
     describe('add', function(){
-        it('Should return error because value does not informed', function(){
-            return transactionBO.add({description: 'Tênis', type: 'out', category: 'Vestuário', date: new Date(), formPayment: 'Débito'})
+        it('Should return error because Description does not informed', function(){
+            return transactionBO.add({value: 33.9, category: 'Vestuário', date: new Date(), formPayment: '507f1f77bcf86cd799439011'})
                 .then()
                 .catch(function(error){
                     expect(error.code).to.be.equals(422);
-                    expect(error.message).to.be.equals('The entity should has a field Value');
+                    expect(error.message).to.be.equals('The entity should has a field description');
                 });
         });
-        it('Should return error because Description does not informed', function(){
-            return transactionBO.add({value: 33.9, type: 'out', category: 'Vestuário', date: new Date(), formPayment: 'Débito'})
+        it('Should return error because Value does not informed', function(){
+            return transactionBO.add({description: 'Tênis', category: 'Vestuário', date: new Date(), formPayment: '507f1f77bcf86cd799439011'})
                 .then()
                 .catch(function(error){
                     expect(error.code).to.be.equals(422);
-                    expect(error.message).to.be.equals('The entity should has a field Description');
+                    expect(error.message).to.be.equals('The entity should has a field value');
+                });
+        });
+        it('Should return error because Category does not informed', function(){
+            return transactionBO.add({description: 'Tênis', value: 33.9, date: new Date(), formPayment: '507f1f77bcf86cd799439011'})
+                .then()
+                .catch(function(error){
+                    expect(error.code).to.be.equals(422);
+                    expect(error.message).to.be.equals('The entity should has a field category');
+                });
+        });
+        it('Should return error because Date does not informed', function(){
+            return transactionBO.add({description: 'Tênis', value: 99.0, category: 'Vestuário', formPayment: '507f1f77bcf86cd799439011'})
+                .then()
+                .catch(function(error){
+                    expect(error.code).to.be.equals(422);
+                    expect(error.message).to.be.equals('The entity should has a field date');
+                });
+        });
+        it('Should return error because FormPayment does not informed', function(){
+            return transactionBO.add({description: 'Tênis', value: 99.0, category: 'Vestuário', date: new Date()})
+                .then()
+                .catch(function(error){
+                    expect(error.code).to.be.equals(422);
+                    expect(error.message).to.be.equals('The entity should has a field formPayment');
                 });
         });
         it('Should return error because out does not informed', function(){
-            return transactionBO.add({description: 'Tênis', value: 33.9, category: 'Vestuário', date: new Date(), formPayment: 'Débito'})
+            return transactionBO.add({description: 'Tênis', value: 99.0, category: 'Vestuário', date: new Date(), formPayment: '527f1f77bcf86cd799439011'})
+                .then()
+                .catch(function(error){
+                    expect(error.code).to.be.equals(404);
+                    expect(error.message).to.be.equals('The formPayment is not found');
+                });
+        });
+
+
+        it('Should return error because out does not informed', function(){
+            return transactionBO.add({description: 'Tênis', value: 99.0, category: 'Vestuário', date: new Date(), formPayment: '507f1f77bcf86cd799439011'})
                 .then()
                 .catch(function(error){
                     expect(error.code).to.be.equals(422);
