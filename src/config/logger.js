b64bcb6aca295b7ea1a69cb286043b26cd74f654
VitorDaynno <vitordaynno@gmail.com>
2019-09-28 00:01:39 -0300

    Adição de método de delete na DAO de transaction

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,11 +1,23 @@
-var winston = require('winston');
+const winston = require('winston')
 
-module.exports = function(){
-  var logger = winston.createLogger({
-    format: winston.format.combine(winston.format.simple()),
+const { format, createLogger, transports } = winston;
+const { printf, combine, colorize, timestamp } = format;
+const { Console, File } = transports;
+
+
+module.exports = () =>{
+
+  const myFormat = printf(({ level, message, timestamp }) => {
+    return `${level}: [${timestamp}]${message}`;
+  });
+
+  const logger = createLogger({
+    format: combine(colorize(),
+                    timestamp({format: 'DD-MM-YYYY HH:mm:ss'}),
+                    myFormat),
     transports: [
-      new winston.transports.Console(),
-      new winston.transports.File({filename: 'error.log', level: 'error'})
+      new Console(),
+      new File({filename: 'error.log', level: 'error'})
     ]
   });
 
