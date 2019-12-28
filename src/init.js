e009b3fe929b50e4a64014273d02762d88bc0944
vitorDaynno <vitordaynno@gmail.com>
2019-12-28 03:42:01 -0200

    Init implementado

--- src/init.js
+++ src/init.js
@@ -1,5 +1,25 @@
-const BOFactory = require('../../factories/factoryBO');
+const logger = require('../config/logger')('Init');
+const BOFactory = require('./factories/factoryBO');
 
 module.exports = function() {
+  logger.info('Starting init');
   const userBO = BOFactory.getBO('user');
+  const chain = Promise.resolve();
+
+  chain
+      .then(function() {
+        logger.info('Verifying if user exist');
+        return userBO.getAll();
+      })
+      .then(function(users) {
+        if (users && users.length === 0) {
+          logger.info('User admin not found and user admin will be created');
+          const user = {
+            name: 'admin',
+            email: 'admin@plutus.com.br',
+            password: '1234',
+          };
+          return userBO.save(user);
+        }
+      });
 };
--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const app = require('./config/express')();
 const database = require('./config/database.js');
+const init = require('./init');
 
 database();
+init();
 
 const server = http.createServer(app).listen(app.get('port'), function() {
   console.log('Express is running on port now ' + app.get('port'));
